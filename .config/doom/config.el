e52291c05ca614ea51c8d803c442757cf5e1672c
Ben Windsor <benjaminwindsor@hotmail.co.uk>
2024-10-20 16:13:29 +0100

    Update org keywords to use NEXT TASK too

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,49 +1,8 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-;; Note this is accompanied by a line in ~/.bashrc of
-;; export BROWSER="powershell.exe /C start"
+
 (setq browse-url-browser-function 'browse-url-generic
  browse-url-generic-program "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe")
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documents/code/personalMonorepo/org")
 (setq gtd-directory (concat org-directory "/gtd"))
 (setq inbox-file (concat gtd-directory "/inbox.org"))
@@ -57,7 +16,6 @@
 (setq projectile-project-search-path '("~/Documents/code/"))
 ;; NOTE: this is NOT recursive, you have to explicitly specify sub directories OR use (setq org-agenda-file (directory-files-recursively org-directory))
 (setq org-agenda-files (list org-directory gtd-directory))
-
 
 (with-eval-after-load 'org
 
@@ -91,7 +49,6 @@
            ("agenda_week.html"))))
 )
 
-
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
@@ -103,7 +60,7 @@
         :desc "Org refile" "w" #'org-refile))
 
 (after! org
-  (setq org-todo-keywords '((sequence "TODO(t)" "IN PROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+  (setq org-todo-keywords '((sequence "TODO(t)" "IN PROGRESS(i)" "NEXT TASK(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
   )
 
 (use-package! org-roam
@@ -177,43 +134,9 @@
           org-roam-ui-update-on-save t
           org-roam-ui-open-on-start t))
 
-;; Mirror secview keybinds because my brain is stuck like that
 (map! "<f9>" #'eval-buffer
       "S-<f9>" #'eval-region
       "M-y" #'magit-status
       "M-b" #'+vertico/switch-workspace-buffer
       "M-e" #'projectile-find-file
       "M-w" #'evil-write)
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
